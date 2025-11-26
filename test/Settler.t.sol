4fa3f139362b88977f414064067e90d177f01858
Solthodox <xabierblockchain@gmail.com>
2025-11-26 12:43:42 +0100

    fix setup

--- test/Settler.t.sol
+++ test/Settler.t.sol
@@ -10,12 +10,10 @@ contract SettlerTest is BaseVaultTest {
     using SafeTransferLib for address;
 
     Settler public settler;
-    MockERC7540 metaVault;
 
     function setUp() public override {
         DeploymentBaseTest.setUp();
 
-        metaVault = new MockERC7540(tokens.usdc, "max usdc", "maxUSDC", 6);
         settler = new Settler(
             users.owner, users.admin, users.relayer, address(minter), address(assetRouter), address(registry)
         );
@@ -37,111 +35,7 @@ contract SettlerTest is BaseVaultTest {
         vm.startPrank(users.admin);
         registry.grantManagerRole(address(settler));
 
-        // IkRegistry(address(registry))
-        //     .setAdapterAllowedSelector(
-        //         address(minterAdapterUSDC),
-        //         address(metaVault),
-        //         2,
-        //         bytes4(keccak256("requestDeposit(uint256,address,address)")),
-        //         true
-        //     );
-        // IkRegistry(address(registry))
-        //     .setAdapterAllowedSelector(
-        //         address(minterAdapterUSDC),
-        //         address(metaVault),
-        //         2,
-        //         bytes4(keccak256("deposit(uint256,address,address)")),
-        //         true
-        //     );
-
-        // IkRegistry(address(registry))
-        //     .setAdapterAllowedSelector(
-        //         address(minterAdapterUSDC),
-        //         address(metaVault),
-        //         2,
-        //         bytes4(keccak256("requestRedeem(uint256,address,address)")),
-        //         true
-        //     );
-
-        IkRegistry(address(registry))
-            .setAdapterAllowedSelector(
-                address(minterAdapterUSDC),
-                address(metaVault),
-                2,
-                bytes4(keccak256("redeem(uint256,address,address)")),
-                true
-            );
-
-        // IkRegistry(address(registry))
-        //     .setAdapterAllowedSelector(
-        //         address(minterAdapterUSDC), address(tokens.usdc), 2, bytes4(keccak256("approve(address,uint256)")), true
-        //     );
-
-        IkRegistry(address(registry))
-            .setAdapterAllowedSelector(
-                address(minterAdapterUSDC), address(metaVault), 2, bytes4(keccak256("approve(address,uint256)")), true
-            );
-
-        IkRegistry(address(registry))
-            .setAdapterAllowedSelector(
-                address(DNVaultAdapterUSDC),
-                address(metaVault),
-                2,
-                bytes4(keccak256("transferFrom(address,address,uint256)")),
-                true
-            );
-
         vm.stopPrank();
-
-        vm.startPrank(users.relayer);
-
-        // First execution: approve USDC to metaVault
-        Execution[] memory executions1 = new Execution[](1);
-        executions1[0] = Execution({
-            target: tokens.usdc,
-            value: 0,
-            callData: abi.encodeWithSignature("approve(address,uint256)", address(metaVault), type(uint256).max)
-        });
-        bytes memory executionCalldata1 = ExecutionLib.encodeBatch(executions1);
-        ModeCode mode1 = ModeLib.encodeSimpleBatch();
-        minterAdapterUSDC.execute(mode1, executionCalldata1);
-
-        // Second execution: approve metaVault to DNVaultAdapter
-        Execution[] memory executions2 = new Execution[](1);
-        executions2[0] = Execution({
-            target: address(metaVault),
-            value: 0,
-            callData: abi.encodeWithSignature(
-                "approve(address,uint256)", address(DNVaultAdapterUSDC), type(uint256).max
-            )
-        });
-        bytes memory executionCalldata2 = ExecutionLib.encodeBatch(executions2);
-        ModeCode mode2 = ModeLib.encodeSimpleBatch();
-        minterAdapterUSDC.execute(mode2, executionCalldata2);
-
-        // Third execution: requestDeposit and deposit
-        uint256 balance = tokens.usdc.balanceOf(address(minterAdapterUSDC));
-        Execution[] memory executions3 = new Execution[](2);
-        executions3[0] = Execution({
-            target: address(metaVault),
-            value: 0,
-            callData: abi.encodeWithSignature(
-                "requestDeposit(uint256,address,address)",
-                balance,
-                address(minterAdapterUSDC),
-                address(minterAdapterUSDC)
-            )
-        });
-        executions3[1] = Execution({
-            target: address(metaVault),
-            value: 0,
-            callData: abi.encodeWithSignature(
-                "deposit(uint256,address,address)", balance, address(minterAdapterUSDC), address(minterAdapterUSDC)
-            )
-        });
-        bytes memory executionCalldata3 = ExecutionLib.encodeBatch(executions3);
-        ModeCode mode3 = ModeLib.encodeSimpleBatch();
-        minterAdapterUSDC.execute(mode3, executionCalldata3);
     }
 
     function test_settler_kminter_empty_batch() public {
@@ -166,12 +60,12 @@ contract SettlerTest is BaseVaultTest {
         bytes32 requestId = minter.requestBurn(tokens.usdc, users.institution, requestAmount);
         assertEq(adapterBalanceAfter - adapterBalanceBefore, depositAmount);
 
-        uint256 metaVaultBalanceBefore = tokens.usdc.balanceOf(address(metaVault));
+        uint256 erc7540USDCBalanceBefore = tokens.usdc.balanceOf(address(erc7540USDC));
         bytes32 proposalId = _closeMinterBatch();
         assertNotEq(proposalId, bytes32(0));
-        uint256 metaVaultBalanceAfter = tokens.usdc.balanceOf(address(metaVault));
+        uint256 erc7540USDCBalanceAfter = tokens.usdc.balanceOf(address(erc7540USDC));
 
-        assertEq(metaVaultBalanceAfter - metaVaultBalanceBefore, netted);
+        assertEq(erc7540USDCBalanceAfter - erc7540USDCBalanceBefore, netted);
         adapterBalanceAfter = tokens.usdc.balanceOf(address(minterAdapterUSDC));
         assertEq(adapterBalanceAfter - adapterBalanceBefore, requestAmount);
 
@@ -202,16 +96,16 @@ contract SettlerTest is BaseVaultTest {
         bytes32 requestId = minter.requestBurn(tokens.usdc, users.institution, requestAmount);
         assertEq(adapterBalanceAfter - adapterBalanceBefore, depositAmount);
 
-        uint256 metaVaultBalanceBefore = tokens.usdc.balanceOf(address(metaVault));
+        uint256 erc7540USDCBalanceBefore = tokens.usdc.balanceOf(address(erc7540USDC));
         bytes32 currentBatchId = minter.getBatchId(tokens.usdc);
         bytes32 proposalId = _closeMinterBatch();
         assertEq(proposalId, bytes32(0));
 
         proposalId = _proposeMinterBatch(currentBatchId);
 
-        uint256 metaVaultBalanceAfter = tokens.usdc.balanceOf(address(metaVault));
+        uint256 erc7540USDCBalanceAfter = tokens.usdc.balanceOf(address(erc7540USDC));
 
-        assertEq(metaVaultBalanceBefore - metaVaultBalanceAfter, nettedNegative);
+        assertEq(erc7540USDCBalanceBefore - erc7540USDCBalanceAfter, nettedNegative);
         adapterBalanceAfter = tokens.usdc.balanceOf(address(minterAdapterUSDC));
 
         assertEq(adapterBalanceAfter - adapterBalanceBefore, requestAmount);
@@ -234,9 +128,9 @@ contract SettlerTest is BaseVaultTest {
         vm.startPrank(users.alice);
         kUSD.approve(address(vault), type(uint256).max);
         bytes32 requestId = vault.requestStake(users.alice, depositAmount);
-        uint256 adapterBalanceBefore = metaVault.balanceOf(address(DNVaultAdapterUSDC));
+        uint256 adapterBalanceBefore = erc7540USDC.balanceOf(address(DNVaultAdapterUSDC));
         bytes32 proposalId = _closeAndProposeDeltaNeutralBatch();
-        uint256 adapterBalanceAfter = metaVault.balanceOf(address(DNVaultAdapterUSDC));
+        uint256 adapterBalanceAfter = erc7540USDC.balanceOf(address(DNVaultAdapterUSDC));
         assertGt(adapterBalanceAfter, adapterBalanceBefore);
         assertEq(assetRouter.getSettlementProposal(proposalId).yield, 0);
 
